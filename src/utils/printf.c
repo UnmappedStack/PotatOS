5bf540ebf4906fd16d26cf187104b621d51e5afa
jakeSteinburger <jake@kendasi.com>
2024-09-05 16:02:14 +1000

    make printf less repetitive

--- src/utils/printf.c
+++ src/utils/printf.c
@@ -3,9 +3,7 @@
 #include "include/printf.h"
 #include <stdarg.h>
 
-void printf(char* format, ...) {
-    va_list args;
-    va_start(args, format);
+void printf_template(char* format, va_list args) {
     size_t i = 0;
     while (i < ku_strlen(format)) {
         if (format[i] == '%') {
@@ -20,8 +18,8 @@ void printf(char* format, ...) {
                 write_serial_char(character);
             } else if (format[i] == 'x') {
                 char bufferx[20];
-                bufferx[19] = 0;
                 ku_uint64_to_hex_string(va_arg(args, uint64_t), bufferx);
+                bufferx[19] = 0;
                 write_serial(bufferx);
             } else if (format[i] == 'b') {
                 char bufferb[65];
@@ -36,45 +34,21 @@ void printf(char* format, ...) {
         }
         i++;
     }
-    va_end(args);
 }
 
+void printf(char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    printf_template(format, args);
+    va_end(args);
+}
 
 // and alternate versions for some different types of messages
 void kstatusf(char* format, ...) {
     write_serial(BYEL "[STATUS] " WHT);
     va_list args;
     va_start(args, format);
-    size_t i = 0;
-    while (i < ku_strlen(format)) {
-        if (format[i] == '%') {
-            i++;
-            char buffer[10];
-            if (format[i] == 'd' || format[i] == 'i') {
-                ku_uint64_to_string(va_arg(args, uint64_t), buffer);
-                buffer[9] = 0;
-                write_serial(buffer);
-            } else if (format[i] == 'c') {
-                char character = va_arg(args, int);
-                write_serial_char(character);
-            } else if (format[i] == 'x') {
-                char bufferx[20];
-                bufferx[19] = 0;
-                ku_uint64_to_hex_string(va_arg(args, uint64_t), bufferx);
-                write_serial(bufferx);
-            } else if (format[i] == 'b') {
-                char bufferb[65];
-                ku_uint64_to_binary_string(va_arg(args, uint64_t), bufferb);
-                bufferb[64] = 0;
-                write_serial(bufferb);
-            } else if (format[i] == 's') {
-                write_serial(va_arg(args, char*));
-            }
-        } else {
-            write_serial_char(format[i]);
-        }
-        i++;
-    }
+    printf_template(format, args);
     va_end(args);
 }
 
@@ -83,36 +57,7 @@ void ktestf(char* format, ...) {
     write_serial(BCYN "[ TEST ] " WHT);
     va_list args;
     va_start(args, format);
-    size_t i = 0;
-    while (i < ku_strlen(format)) {
-        if (format[i] == '%') {
-            i++;
-            char buffer[10];
-            if (format[i] == 'd' || format[i] == 'i') {
-                ku_uint64_to_string(va_arg(args, uint64_t), buffer);
-                buffer[9] = 0;
-                write_serial(buffer);
-            } else if (format[i] == 'c') {
-                char character = va_arg(args, int);
-                write_serial_char(character);
-            } else if (format[i] == 'x') {
-                char bufferx[20];
-                bufferx[19] = 0;
-                ku_uint64_to_hex_string(va_arg(args, uint64_t), bufferx);
-                write_serial(bufferx);
-            } else if (format[i] == 'b') {
-                char bufferb[65];
-                ku_uint64_to_binary_string(va_arg(args, uint64_t), bufferb);
-                bufferb[64] = 0;
-                write_serial(bufferb);
-            } else if (format[i] == 's') {
-                write_serial(va_arg(args, char*));
-            }
-        } else {
-            write_serial_char(format[i]);
-        }
-        i++;
-    }
+    printf_template(format, args);
     va_end(args);
 }
 
@@ -120,36 +65,7 @@ void kdebugf(char* format, ...) {
     write_serial(BMAG "[KDEBUG] " WHT);
     va_list args;
     va_start(args, format);
-    size_t i = 0;
-    while (i < ku_strlen(format)) {
-        if (format[i] == '%') {
-            i++;
-            char buffer[10];
-            if (format[i] == 'd' || format[i] == 'i') {
-                ku_uint64_to_string(va_arg(args, uint64_t), buffer);
-                buffer[9] = 0;
-                write_serial(buffer);
-            } else if (format[i] == 'c') {
-                char character = va_arg(args, int);
-                write_serial_char(character);
-            } else if (format[i] == 'x') {
-                char bufferx[20];
-                bufferx[19] = 0;
-                ku_uint64_to_hex_string(va_arg(args, uint64_t), bufferx);
-                write_serial(bufferx);
-            } else if (format[i] == 'b') {
-                char bufferb[65];
-                ku_uint64_to_binary_string(va_arg(args, uint64_t), bufferb);
-                bufferb[64] = 0;
-                write_serial(bufferb);
-            } else if (format[i] == 's') {
-                write_serial(va_arg(args, char*));
-            }
-        } else {
-            write_serial_char(format[i]);
-        }
-        i++;
-    }
+    printf_template(format, args);
     va_end(args);
 }
 
@@ -157,35 +73,6 @@ void kfailf(char* format, ...) {
     write_serial(BRED "[ FAIL ] " WHT);
     va_list args;
     va_start(args, format);
-    size_t i = 0;
-    while (i < ku_strlen(format)) {
-        if (format[i] == '%') {
-            i++;
-            char buffer[10];
-            if (format[i] == 'd' || format[i] == 'i') {
-                ku_uint64_to_string(va_arg(args, uint64_t), buffer);
-                buffer[9] = 0;
-                write_serial(buffer);
-            } else if (format[i] == 'c') {
-                char character = va_arg(args, int);
-                write_serial_char(character);
-            } else if (format[i] == 'x') {
-                char bufferx[20];
-                bufferx[19] = 0;
-                ku_uint64_to_hex_string(va_arg(args, uint64_t), bufferx);
-                write_serial(bufferx);
-            } else if (format[i] == 'b') {
-                char bufferb[65];
-                ku_uint64_to_binary_string(va_arg(args, uint64_t), bufferb);
-                bufferb[64] = 0;
-                write_serial(bufferb);
-            } else if (format[i] == 's') {
-                write_serial(va_arg(args, char*));
-            }
-        } else {
-            write_serial_char(format[i]);
-        }
-        i++;
-    }
+    printf_template(format, args);
     va_end(args);
 }
